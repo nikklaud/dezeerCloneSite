7a38958a344a6f47217ce04d02b770bcb209c5a1
nikklaud <titnik47@gmail.com>
2024-10-20 15:40:51 +0500

    add jinja config

--- friduzho/plans/urls.py
+++ friduzho/plans/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
+from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", )
+    path("", views.plans)
 ]
